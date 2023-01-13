428ff01321b65f2861ab4aeb4c480d767fb6afa0
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2023-01-13 13:16:55 +0000

    Work on incorporating use inputs into building queryURL and setting a default when no cuisine selected to ensure user gets recipe results if they don't enter anything into the inputs and just click search

--- assets/script.js
+++ assets/script.js
@@ -11,55 +11,44 @@ let intoleranceCheckboxes = document.forms["search-criteria-form"];
 
 let recipeResultsSec = $("#recipe-results-section");
 
-//Intolerances
-// let noIntolerancesCheckbox = $(".no-intolerances");
-// let dairyIntoleranceCheckbox = $(".dairy");
-// let eggIntoleranceCheckbox = $(".egg");
-// let glutenIntoleranceCheckbox = $(".gluten");
-// let grainIntoleranceCheckbox = $(".grain");
-// let seafoodIntoleranceCheckbox = $(".seafood");
-// let shellfishIntoleranceCheckbox = $(".shellfish");
-// let soyIntoleranceCheckbox = $(".soy");
-// let sulfiteIntoleranceCheckbox = $(".sulfite");
-// let treenutIntoleranceCheckbox = $(".treenut");
-// let wheatIntoleranceCheckbox = $(".wheat");
-
 // function to generate query url
 function printUserInputsToConsole(event) {
   event.preventDefault();
-  // How do I use intolerance as a parameter in query URL? A comma-separated list of intolerances
-  // How do I get the values from checkboxes
-  // let noIntolerance = noIntolerancesCheckbox.val();
-  // let dairyIntolerance = dairyIntoleranceCheckbox.val();
-  // let eggIntolerance = eggIntoleranceCheckbox.val();
-  // let glutenIntolerance = glutenIntoleranceCheckbox.val();
 
-  //let allIntolerances = $("input[name='intolerance']:checked");
+  // How do I use intolerance as a parameter in query URL? A comma-separated list of intolerances
+  // How do I use cuisine as a parameter in query URL? cuisine=italian
+  // How do I use diet as a parameter in query URL? diet=vegan OR where there are two words you can use a space to seperate them e.g. diet=low foodmap
+  let queryURL = `https://api.spoonacular.com/recipes/complexSearch?&number=7&type=main&addRecipeInformation=true&addRecipeNutrition=true&apiKey=${APIKey}&diet=low fodmap&intolerances=peanut`;
   let selectedIntolerances = $("input[name='intolerance']:checked")
     .map(function () {
       return $(this).val();
     })
     .get();
   let intolerancesString = selectedIntolerances.join(",");
-
   let specialDiet = $("#diet").val().toLowerCase();
-
-  // let diet = dietDropdownBtn.val();
-  // let cuisine = cuisineDropdownBtn.val();
+  let cuisine = $("#cuisine").val().toLowerCase();
   let timeToPrep = timeToPrepInputEl.val();
+  let timeToPrepQueryText = timeToPrep > 0 ? `&timeToPrep=${timeToPrep}` : "";
+  let specialDietQueryText = specialDiet ? `&diet=${specialDiet}` : "";
+  let cuisineQueryText = cuisine
+    ? `&cuisine=${cuisine}`
+    : "&cuisine=african,american,british,cajun,caribbean,chinese,eastern european,european,french,german,greek,indian,irish,italian,japanese,jewish,korean,latin american,mediterranean,mexican,middle eastern,nordic,southern,spanish,thai,vietnamese";
 
+  let userInputQueryURL = `https://api.spoonacular.com/recipes/complexSearch?&number=7&type=main&addRecipeInformation=true&addRecipeNutrition=true&apiKey=${APIKey}&intolerances=${intolerancesString}${specialDietQueryText}${cuisineQueryText}${timeToPrepQueryText}`;
   console.log(intolerancesString);
   console.log(specialDiet);
   console.log(timeToPrep);
+  console.log(cuisine);
+  console.log(userInputQueryURL);
 }
 
 let APIKey = "2f346a836aae470092494ca66fe7f8fa";
 
 // queryURL for searching recipes
-let queryURL = `https://api.spoonacular.com/recipes/complexSearch?&number=7&type=main&addRecipeInformation=true&addRecipeNutrition=true&apiKey=${APIKey}&intolerances=peanut`;
+let queryURL = `https://api.spoonacular.com/recipes/complexSearch?&number=7&type=main&addRecipeInformation=true&addRecipeNutrition=true&apiKey=${APIKey}&diet=low fodmap&intolerances=peanut`;
 
 // Checking results for diet and intolerances by hardcoding URL
-// let queryURL = `https://api.spoonacular.com/recipes/complexSearch?&apiKey=${APIKey}&diet=pescetarian&intolerances=egg`;
+// let queryURL = `https://api.spoonacular.com/recipes/complexSearch?&apiKey=${APIKey}&diet=pescetarian&intolerances=egg&maxReadyTime=25`;
 
 //This query url includes the following variables we can search by:
 // includeIngredients - A comma-separated list of ingredients that should/must be used in the recipes.
@@ -87,6 +76,8 @@ $.ajax({
   console.log(response);
   let mealTitle = response.results[0].title;
   let mealID = response.results[0].id;
+  let totalResults = response.totalResults;
+  console.log(totalResults);
   //Need to figure out what unit price is measured in and display accordingly
   let pricePerServing = response.results[0].pricePerServing;
   let readyInMinutes = response.results[0].readyInMinutes;
