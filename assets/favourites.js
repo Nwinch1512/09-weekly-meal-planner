6144d963aab9711c09735381cded9cc29ea94303
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2023-01-17 00:05:39 +0000

    Added some styling and minor text changes to pages

--- assets/favourites.js
+++ assets/favourites.js
@@ -10,8 +10,8 @@ let tableContainer = $("#favourites-table")
 // Function to append   
 function appendRows(){
 
-    // Deleting the buttons prior to adding new movies
-      // (this is necessary otherwise there will be repeat buttons)
+    // Deleting rows prior to adding new movies
+      // (this may be necessary otherwise there will be repeat rows when row removed function is used)
       // tableContainer.empty();
     
       // Take array of locations from local storage and loop through them
@@ -34,41 +34,24 @@ function appendRows(){
 
         var columnTwo = $("<div>")
         .addClass("col-sm d-flex justify-content-center entries-columns");
+        // Add meal image to this column
 
         var columnThree = $("<div>")
         .addClass("col-sm d-flex justify-content-center entries-columns");
+        // Add button that links to blog to this column
 
         var columnFour = $("<div>")
         .addClass("col-sm d-flex justify-content-center entries-columns");
+        // Add button to allow user to remove this item from favourites
 
         newRow.append(columnOne, columnTwo, columnThree, columnFour);
 
         tableContainer.append(newRow);
 
 
-
-        // columnOne.innerHTML += `<p>${favouriteIDs[i]}</p>`
-
-
-
-
-        /* // Then dynamicaly generating buttons for each location in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        var newBtn = $("<button>");
-        // Adding a type to the button
-        // newBtn.addType("button");
-        // Adding a class of movie to our button
-        newBtn.addClass("btn btn-secondary btn-block w-100 rounded mt-3 added-buttons");
-        // Adding a data-attribute
-        newBtn.attr("data-name", locations[i]);
-        // Providing the initial button text
-        newBtn.text(locations[i]);
-        // Adding the button to the "list-group" div
-        $(".list-group").append(newBtn); */
-      }
       
     };
-    
+}
     
     // Append rows from local storage when page is reloaded
     appendRows();
