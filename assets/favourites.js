65763bdca96d1ed36c481c297d432544671e14e0
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2023-01-17 21:21:35 +0000

    Remove button added

--- assets/favourites.js
+++ assets/favourites.js
@@ -29,7 +29,8 @@ function appendRows(){
 
         var columnOne = $("<div>")
         .addClass("col-sm d-flex justify-content-center entries-columns")
-        .text(`${mealTitles[i]}`);
+        .text(`${mealTitles[i]}`)
+        .css("font-size", "20px");
 
         var columnTwo = $("<div>")
         .addClass("col-sm d-flex justify-content-center entries-columns");
@@ -44,11 +45,39 @@ function appendRows(){
 
         var columnThree = $("<div>")
         .addClass("col-sm d-flex justify-content-center entries-columns");
+        
         // Add button that links to blog to this column
+        let linkDiv = $("<div>");
+
+        let buttonLink = $("<button>")
+          .addClass("btn btn-danger")
+          // add attribute as an identifier for favourite button
+          .attr("urlBlog", blogURLs[i])
+          .text("Blog Link")
+          .css("background-color", "#9A031E")
+          .css("border-color", "#9A031E");
+          
+          linkDiv.append(buttonLink);
+          columnThree.append(linkDiv);
 
         var columnFour = $("<div>")
         .addClass("col-sm d-flex justify-content-center entries-columns");
+       
         // Add button to allow user to remove this item from favourites
+        let removeDiv = $("<div>");
+
+        let buttonRemove = $("<button>")
+          .addClass("btn btn-danger")
+          // add attribute as an identifier for favourite button
+          .attr("id", favouriteIDs[i])
+          .text("X")
+          .css("background-color", "#9A031E")
+          .css("border-color", "#9A031E");
+          
+          removeDiv.append(buttonRemove);
+          columnFour.append(removeDiv);
+
+
 
         newRow.append(columnOne, columnTwo, columnThree, columnFour);
 
@@ -61,3 +90,29 @@ function appendRows(){
     
     // Append rows from local storage when page is reloaded
     appendRows();
+
+
+/*     // Code to control opening and closing of modal
+
+// Get the modal div from HTML
+var modal = $("#myModal");
+
+// Get the <span> element that closes the modal
+var span = $(".close")[0];
+
+// When the user clicks the cooking button, open the modal. This function is used
+// so appended buttons added by JS to document can be clicked on
+
+$(document).on("click", ".modal-btns", openModal);
+
+// Open Modal then
+function openModal(event) {
+  event.preventDefault();
+  modal.css("display", "block");
+  iframeWebsite($(this).attr("url"));
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.css("display", "none");
+}; */
